0235c4f94f004bcf3cbea3a475681b931398113b
dardoflorian <walo0209@gmail.com>
2017-11-07 14:01:31 -0600

    Archivo TDM_output.root

--- TDM_G4-0.2/src/TDM_EventAction.cc
+++ TDM_G4-0.2/src/TDM_EventAction.cc
@@ -2,6 +2,7 @@
 #include "TDM_Run.hh"
 #include "TDM_RunAction.hh"
 #include "TDM_PrimaryGeneratorAction.hh"
+#include "TDM_Analysis.hh"
 
 #include "G4Event.hh"
 #include "G4RunManager.hh"
@@ -13,9 +14,7 @@ TDM_EventAction::TDM_EventAction()
 : G4UserEventAction()
 {
     fAbsoEdepHCID = -1;
-    fGapEdepHCID = -1;
     fAbsoTrackLengthHCID = -1;
-    fGapTrackLengthHCID = -1;
     fAbsoDodepHCID = -1;
 }
 
@@ -71,12 +70,7 @@ void TDM_EventAction::PrintEventStatistics(
 	 << "       total dose deposit: "
 	 << std::setw(7) << G4BestUnit(absoDodep, "Dose")
 	 << G4endl;
- /*    << "        Gap: total energy: "
-     << std::setw(7) << G4BestUnit(gapEdep, "Energy")
-     << "       total track length: "
-     << std::setw(7) << G4BestUnit(gapTrackLength, "Length")
-     << G4endl;
-*/
+
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -93,47 +87,26 @@ void TDM_EventAction::EndOfEventAction(const G4Event* event)
 	      = G4SDManager::GetSDMpointer()->GetCollectionID("Absorber/Edep");
 	    fAbsoDodepHCID
 	    	      = G4SDManager::GetSDMpointer()->GetCollectionID("Absorber/Dodep");
-//	    fGapEdepHCID
-//	      = G4SDManager::GetSDMpointer()->GetCollectionID("Gap/Edep");
 	    fAbsoTrackLengthHCID
 	      = G4SDManager::GetSDMpointer()->GetCollectionID("Absorber/TrackLength");
-//	    fGapTrackLengthHCID
-//	      = G4SDManager::GetSDMpointer()->GetCollectionID("Gap/TrackLength");
-	    /*fAbsoDodepHCID
-	   	      = G4SDManager::GetSDMpointer()->GetCollectionID("Absorber/Dodep");
-	  */
 	  }
 
 	  // Get sum values from hits collections
 	  //
 	  auto absoEdep = GetSum(GetHitsCollection(fAbsoEdepHCID, event));
 	  auto absoDodep = GetSum(GetHitsCollection(fAbsoDodepHCID, event));
-
-//	  auto gapEdep = GetSum(GetHitsCollection(fGapEdepHCID, event));
-
 	  auto absoTrackLength
 	    = GetSum(GetHitsCollection(fAbsoTrackLengthHCID, event));
-//	  auto gapTrackLength
-//	    = GetSum(GetHitsCollection(fGapTrackLengthHCID, event));
-
 	  // get analysis manager
-	 /* auto analysisManager = G4AnalysisManager::Instance();
+	  auto analysisManager = G4AnalysisManager::Instance();
 
-	  // fill histograms
-	  //
-	  analysisManager->FillH1(0, absoEdep);
-	  analysisManager->FillH1(1, gapEdep);
-	  analysisManager->FillH1(2, absoTrackLength);
-	  analysisManager->FillH1(3, gapTrackLength);
 
 	  // fill ntuple
 	  //
 	  analysisManager->FillNtupleDColumn(0, absoEdep);
-	  analysisManager->FillNtupleDColumn(1, gapEdep);
-	  analysisManager->FillNtupleDColumn(2, absoTrackLength);
-	  analysisManager->FillNtupleDColumn(3, gapTrackLength);
-	  analysisManager->AddNmtupleRow();
-*/
+	  analysisManager->FillNtupleDColumn(1, absoDodep);
+	  analysisManager->AddNtupleRow();
+
 	  //print per event (modulo n)
 	  //
 	  auto eventID = event->GetEventID();
